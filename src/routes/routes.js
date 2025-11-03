96975f12930664f37755a5565fec6086a2a05f35
brayan141 <44105405+brayan141@users.noreply.github.com>
2025-11-03 09:27:33 -0500

    Login completo
    Login completo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = express();
 
-const bookController = require('../controllers/book.controller');
 const authController = require('../controllers/auth.controller');
+const { authenticateToken } = require('../middleware/jwt');
 
 module.exports = () => {
     router.post('/auth/login', authController.login);
     router.post('/auth/register', authController.register);
-    router.put('/auth/update-password', authController.updatePassword);
-    router.get('/books', bookController.getBooks);
-    router.get('/books/:id', bookController.getBookById);
-    router.post('/books', bookController.createBook);
-    router.put('/books/:id', bookController.updateBookById);
-    router.delete('/books/:id', bookController.deleteBookById);
-    router.get('/prueba', bookController.getPrueba);
+    router.put('/auth/update-password', authenticateToken, authController.updatePassword);
+    router.get('/auth/users', authenticateToken, authController.getUsers);
 
     return router;
 }
